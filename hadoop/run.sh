25d8057a354affbbcfa1eebca7177f056b11e892
Martin Durant <mdurant@sri.utoronto.ca>
2017-08-15 10:24:14 -0400

    Working hdfs/yarn
    (for actual usage, should create non-priviliged user)

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -1,3 +1,11 @@
+service ssh restart
+ssh-keygen -t dsa -P '' -f ~/.ssh/id_dsa
+cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+echo 'NoHostAuthenticationForLocalhost yes' > ~/.ssh/config
+echo 'export JAVA_HOME='$JAVA_HOME | cat - /opt/hadoop/etc/hadoop/hadoop-env.sh > temp
+rm /opt/hadoop/etc/hadoop/hadoop-env.sh 
+mv temp /opt/hadoop/etc/hadoop/hadoop-env.sh
+chmod 0600 ~/.ssh/authorized_keys
 start-dfs.sh
 start-yarn.sh
 sleep infinity
